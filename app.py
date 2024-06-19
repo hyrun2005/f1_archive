b96842047840fe21306491f78a63f5958908eded
Yurii Hyrun <80849634+hyrun2005@users.noreply.github.com>
2024-06-19 21:09:08 +0200

    adding page - about driver

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, render_template, request
 import requests
+import json
 app = Flask(__name__)
 
 
@@ -18,18 +19,18 @@ def get_drivers():
     else:
         return f"Error fetching data: {response.status_code}"
 
-@app.route('/drivers/<famName>')
-def more_info(famName):
-    response = requests.get(f"{ERGAST_API_URL}/")
+@app.route('/drivers/<number>')
+def more_info(number):
+    response = requests.get(f"https://api.openf1.org/v1/drivers?driver_number={number}&session_key=9158")
     if response.status_code == 200:
         data = response.json()
-        drivers = data['MRData']['DriverTable']['Drivers']
-        return render_template('drivers.html', drivers=drivers, current_year=year)
+        full = ""
+        return render_template('driver_info.html', driver=data, photo=full)
     else:
         return f"Error fetching data: {response.status_code}"
 
 
-@app.route('/main_page')
+@app.route('/')
 def main_page_handle():
     return render_template('main_page.html')
 
